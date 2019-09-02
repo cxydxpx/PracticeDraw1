4ae3b1371e5d5a75dd69fc939fc2ab5a1089e1cd
cxydxpx <cxydxpx@163.com>
2019-09-02 10:28:19 +0800

    ++++++

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,11 +22,33 @@ public class Practice10HistogramView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+
+    private Paint mPaint = new Paint();
+
+    private float[] mLoca = {
+            200,100,200,500,
+            200,500,900,500
+    };
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+        mPaint.setColor(Color.parseColor("#ffffff"));
+        canvas.drawLines(mLoca,mPaint);
+
+        mPaint.setTextSize(30);
+        canvas.drawText("froyo",250,520,mPaint);
+
+        mPaint.setColor(Color.BLUE);
+        canvas.drawRect(250,400,300,500,mPaint);
+
+        mPaint.setColor(Color.WHITE);
+        canvas.drawText("GB",400,520,mPaint);
+//        canvas.drawRect();
+        mPaint.setColor(Color.BLUE);
+        canvas.drawRect(400,100,450,500,mPaint);
+
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,6 +24,7 @@ public class Practice11PieChartView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
 
 //        综合练习
 //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
